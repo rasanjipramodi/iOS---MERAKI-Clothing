0cce180c5f73921958551ff7516d75810c9ed4c7
adeepadevinda <127526750+rasanjipramodi@users.noreply.github.com>
2024-03-11 23:00:53 +0530

    Initial Commit

---
+++ iOS - MERAKI Clothing/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iOS - MERAKI Clothing
+//
+//  Created by Adeepa on 2024-03-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
